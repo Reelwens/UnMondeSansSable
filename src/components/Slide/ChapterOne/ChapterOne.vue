86f5af5e2eeef27e0a069b112e14f51b9f550684
AntoineMoues <antoine.moues@gmail.com>
2017-06-28 16:58:04 +0200

    :rocket: Slider is working
    Router and slider are working, doing the navbar

--- src/components/Slide/ChapterOne/ChapterOne.vue
+++ src/components/Slide/ChapterOne/ChapterOne.vue
@@ -2,45 +2,57 @@
 
 <template>
   <div class="slide-container">
-    <div class="slides-container">
+    <div class="slides-container" v-bind:data-slide="slide_index">
       <div class="slides slide-1">
         <div class="slide-content-container">
-          <video src="../../../../static/videos/chapter-1.mp4" autoplay poster="../../../assets/images/home/background-1.jpg">
-
-          </video>
+          <video src="../../../../static/videos/chapter-1.mp4" autoplay loop poster="../../../assets/images/home/background-1.jpg"></video>
         </div>
       </div>
       <div class="slides slide-2">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
       <div class="slides slide-3">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
       <div class="slides slide-4">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
       <div class="slides slide-5">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
       <div class="slides slide-6">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
       <div class="slides slide-7">
         <div class="slide-content-container">
-          
+
         </div>
       </div>
     </div>
+    <footer>
+      <div class="footer-title">
+        <h2>Chapitre 1</h2>
+        <h2 class="part">Avant la pénurie de l'eau</h2>
+      </div>
+      <div class="footer-links">
+        <div class="footer-links-container" v-bind:data-slide="slide_index">
+          <div class="footer-link footer-link-1">
+            <div class="footer-link-inner">
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -64,8 +76,6 @@ export default {
 
     let lethargy = new Lethargy()
 
-    let y = 1
-
     let _this = this
 
     window.addEventListener('mousewheel', (e) => {
@@ -87,7 +97,7 @@ export default {
   methods: {
     slide_down() {
 
-      if(this.slide_index != 7){
+      if(this.slide_index != 6){
           this.slide_index += 1
       }
       this.scroll_control()
